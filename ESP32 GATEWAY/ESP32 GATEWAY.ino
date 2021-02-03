5c87c2cdcb2dcf01632ad98e9c96304aea6b0def
BuiThien <bui.trong.thien.92@gmail.com>
2021-02-03 15:56:11 +0700

    Init Project

---
+++ ESP32 GATEWAY/ESP32 GATEWAY.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       ESP32 GATEWAY.ino
+    Created:	03-Feb-21 3:54:33 PM
+    Author:     DESKTOP-0BVOA2M\Bui Trong Thien
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
